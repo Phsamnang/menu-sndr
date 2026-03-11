9b3c74568d18a2157a02a34d900ea794c342ab25
somnang <samnangldn@gmail.com>
2026-03-11 19:48:31 +0700

    update

--- app/admin/orders/[orderId]/components/OrderCartSidebar.tsx
+++ app/admin/orders/[orderId]/components/OrderCartSidebar.tsx
@@ -715,7 +715,7 @@ export default function OrderCartSidebar({
                     d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
                   />
                 </svg>
-                <span className="hidden xs:inline">បោះពុម្ព</span>
+                <span className="inline">បោះពុម្ព</span>
               </button>
             </div>
           ) : (
@@ -738,7 +738,7 @@ export default function OrderCartSidebar({
                     d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
                   />
                 </svg>
-                <span className="hidden xs:inline">បោះពុម្ព</span>
+                <span className="inline">បោះពុម្ព</span>
               </button>
               <button
                 onClick={handlePlaceOrder}
